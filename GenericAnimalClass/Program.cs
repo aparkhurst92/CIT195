69810672cd8a87030d9246d0f5c853b25587c2b2
Ainsley <parkhua@mail.nmc.edu>
2025-02-25 13:13:19 -0500

    message

---
+++ GenericAnimalClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Animal<T>
+{
+    public T data;
+
+    public Animal(T data)
+    {
+        this.data = data;
+        Console.WriteLine("Data passed: " + this.data);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal<string> animalName = new Animal<string>("Fido");
+
+        Animal<string> animalHabitat = new Animal<string>("Grassland");
+
+        Animal<int> averageLifespan = new Animal<int>(10);
+
+        Animal<bool> endangered = new Animal<bool>(true);
+
+        Animal<bool> extinct = new Animal<bool>(false);
+    }
+}
